bcc4c565042a89cf4dce4a2839bc625274109826
Giovanny <diasgiovanny@gmail.com>
2023-02-08 12:30:33 +0100

    addEventListener produit avec création d'un objet

--- front/html/products.js
+++ front/html/products.js
@@ -4,7 +4,10 @@ const productId = params.get("id");
 fetch(`http://localhost:3000/api/products/${productId}`)
   .then(response => response.json())
   .then(product => {
-    
+
+    // je modifie le nom du document par le nom du produit
+    document.title = product.name;
+
     // je crée l'image grâce à l'objet et je la met dans le DOM rattaché à la div item__img
     const imageProduit = document.createElement("img");
     imageProduit.setAttribute('src', product.imageUrl);
@@ -24,9 +27,37 @@ fetch(`http://localhost:3000/api/products/${productId}`)
       newOption.setAttribute('value', color);
       newOption.innerText = color;
 
-      document.getElementById('colors').appendChild(newOption);
-      
+      // Je rajoute le ou les options dans l'élements select du document
+      document.getElementById('colors').appendChild(newOption);  
   });
 
-  })
+  const select = document.getElementById('colors');
+  const ajouterPanier = document.getElementById('addToCart');
+  const quantite = document.getElementById('quantity');  
+
+  let articleId = params.get("id");
+
+  ajouterPanier.addEventListener('click', () => {
+    let panier = [];
+
+    // je récupère l'index de la sélection et je stock sa valeur dans une variable
+    const optionIndex = select.options[select.selectedIndex];
+    const optionSelected = optionIndex.value;
+
+    // je stock la valeur de la quantitié dans une variable
+    const quantity = document.getElementById('quantity');
+    const quantitySelected = quantity.value;
+
+    let article = {
+      id: articleId,
+      option: optionSelected,
+      quantity: quantitySelected
+    }
+
+    panier.push(article);
+    console.log(article);
+
+  });
+
+})
   .catch(error => console.error(error));
