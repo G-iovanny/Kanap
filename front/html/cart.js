a9f744a17e35c6db793d93b34e5d271442e1486f
Giovanny <diasgiovanny@gmail.com>
2023-02-22 02:33:24 +0100

    Finition des vérifications d'input

--- front/html/cart.js
+++ front/html/cart.js
@@ -2,42 +2,52 @@ fetch('http://localhost:3000/api/products')
   .then(response => response.json())
   .then(products => {
 
-    //Je récupère mon panier dans le localstorage
-    let panier = JSON.parse(localStorage.getItem('panier'));
+    // Je récupère le panier dans le localStorage
+    const panier = JSON.parse(localStorage.getItem('panier'));
 
-    // J'initialise mon panier qui sera sous forme HTML
-    let cartItems = '';
-    let totalPrice = 0;
-    let totalQuantity = 0;
-    // Je crée une boucle pour chaque produit du panier en récupérant les valeurs dont j'ai besoin
-    for (let i = 0; i < panier.length; i++) {
-      let productId = panier[i].id;
-      // Je cherche l'objet correspondant à l'ID du panier et je le met dans la variable product
-      let product = products.find(p => p._id === productId);
-      let imgUrl = product.imageUrl;
-      let price = product.price;
-      let color = panier[i].option;
-      let quantity = panier[i].quantity;
-      let fileName = imgUrl.split('/').pop();
-      totalPrice += price * quantity;
-      totalQuantity += quantity;
+    function getElementsOnStorage() {
+      const elementsArray = []; // Tableau pour stocker les éléments du panier
+      
+      // Boucle sur chaque item du panier
+      for (let item of panier) {
+        const product = products.find(p => p._id === item.id); // Chercher l'objet correspondant à l'ID du panier
+        // Création d'un objet avec toutes les propriétés nécessaires pour chaque élement
+        const element = {
+          productId: item.id,
+          name: product.name,
+          color: item.option,
+          price: product.price,
+          image: product.imageUrl.split('/').pop(),
+          quantity: item.quantity,
+          txtAlt : product.altTxt
+        };
+        elementsArray.push(element);
+      }
+    
+      return elementsArray;
+    }
+    const elements = getElementsOnStorage();
 
-      // À chaque tour de boucle je rajoute mon produit dans le HTML avec ses propriétés
-      cartItems += `
-        <article class="cart__item" data-id="${productId}" data-color="${color}">
+    // On initialise la variable article en dehors d'une fonction pour la réutiliser où on veut
+    let article = "";
+
+    function createArticles(elements){
+      for(let i = 0; i < elements.length; i++){
+        article +=
+        `<article class="cart__item" data-id="${elements[i].productId}" data-color="${elements[i].color}">
           <div class="cart__item__img">
-            <img src="../../back/images/${fileName}" alt="Photographie d'un canapé">
+            <img src="../../back/images/${elements[i].image}" alt="${elements[i].txtAlt}">
           </div>
           <div class="cart__item__content">
             <div class="cart__item__content__description">
-              <h2>${product.name}</h2>
-              <p>${color}</p>
-              <p>${price} €</p>
+              <h2>${elements[i].name}</h2>
+              <p>${elements[i].color}</p>
+              <p>${elements[i].price} €</p>
             </div>
             <div class="cart__item__content__settings">
               <div class="cart__item__content__settings__quantity">
                 <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${elements[i].quantity}">
               </div>
               <div class="cart__item__content__settings__delete">
                 <p class="deleteItem">Supprimer</p>
@@ -45,17 +55,112 @@ fetch('http://localhost:3000/api/products')
             </div>
           </div>
         </article>`;
+      }
+    }
+    createArticles(elements);
+    document.getElementById('cart__items').innerHTML = article;
 
-        
-    };
+    function updateLocalStorageQuantity() {
+      const quantityInputs = document.querySelectorAll('.itemQuantity');
+    
+      quantityInputs.forEach(input => {
+        input.addEventListener('change', () => {
+          const id = input.closest('.cart__item').dataset.id;
+          const panier = JSON.parse(localStorage.getItem('panier'));
+          const item = panier.find(item => item.id === id);
+          
+          // Vérification de la valeur de quantité
+          let quantity = parseInt(input.value);
+          if (isNaN(quantity) || quantity < 1 || quantity > 100) {
+            alert('Veuillez entrer une quantité valide (entre 1 et 100)');
+            input.value = item.quantity;
+            return;
+          }
+          
+          item.quantity = quantity;
+          localStorage.setItem('panier', JSON.stringify(panier));
+        });
+      });
+    }
+  updateLocalStorageQuantity()   
 
-    // J'ajoute mon HTML à la section
-    const section = document.getElementById('cart__items');
-    section.innerHTML = cartItems;
+  function deleteItem(event) {
+    const item = event.target.closest('.cart__item');
+    const id = item.dataset.id;
+    const option = item.dataset.color;
+    const index = panier.findIndex(item => item.id === id && item.option === option);
+    if (index !== -1) {
+      panier.splice(index, 1);
+      localStorage.setItem('panier', JSON.stringify(panier));
+      item.remove();
+    }
+  }
+  
+  document.querySelectorAll('.deleteItem').forEach(button => {
+    button.addEventListener('click', deleteItem);
+  });
+  
+  function validateNamesCity(e) {
+    const namePattern = /^[a-zA-ZÀ-ÿ'-]+$/;
+    return namePattern.test(e.trim());
+  }
+  
+  function validateAddress(address) {
+    const addressPattern = /^[a-zA-Z0-9\s,'-]+$/;
+    return addressPattern.test(address.trim());
+  }
 
-    document.getElementById('totalPrice').innerText = totalPrice;
-    document.getElementById('totalQuantity').innerText = totalQuantity;
+  function validateMail(mail){
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(mail.trim())
+  }
+  
+  document.getElementById('order').addEventListener('click', function(event){
 
-    console.log(totalPrice);
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const mail = document.getElementById("email").value;
+
+
+    
+    if (!validateNamesCity(firstName)) {
+      event.preventDefault();
+      console.log("Erreur dans le prénom");
+      document.getElementById('firstNameErrorMsg').innerText = "Champ mal rempli";
+    }
+    
+    if (!validateNamesCity(lastName)) {
+      event.preventDefault();
+      console.log("Erreur dans le nom");
+      document.getElementById('lastNameErrorMsg').innerText = "Champ mal rempli";
+    }
+    
+    
+    if (!validateAddress(address)) {
+      event.preventDefault();
+      console.log("Erreur dans l'adresse");
+      document.getElementById('addressErrorMsg').innerText = "Champ mal rempli";
+    }
+
+    if (!validateNamesCity(city)) {
+      event.preventDefault();
+      console.log("Erreur dans la ville");
+      document.getElementById('cityErrorMsg').innerText = "Champ mal rempli";
+    }
+    if (!validateMail(mail)) {
+      event.preventDefault();
+      console.log("Erreur dans l'email");
+      document.getElementById('emailErrorMsg').innerText = "Champ mal rempli";
+    }
+  });
+  
+  
+  
+
+
+   
+  
   })
   .catch(error => console.log(error));
--- front/html/products.js
+++ front/html/products.js
@@ -2,93 +2,99 @@ const params = new URLSearchParams(window.location.search);
 const productId = params.get("id");
 
 fetch(`http://localhost:3000/api/products/${productId}`)
-  .then(response => response.json())
-  .then(product => {
+   .then(response => response.json())
+   .then(product => {
 
-    // je modifie le nom du document par le nom du produit
-    document.title = product.name;
+      function updatePage(product) {
 
-    // je crée l'image grâce à l'objet et je la met dans le DOM rattaché à la div item__img
-    const imageProduit = document.createElement("img");
-    imageProduit.setAttribute('src', product.imageUrl);
-    imageProduit.setAttribute('alt', product.altTxt)
-    document.querySelector('.item__img').appendChild(imageProduit);
+         document.title = product.name; // je modifie le nom du document par le nom du produit
 
-    // Je récupère le H1 pour y insérer le nom du produit
-    document.getElementById('title').innerText = product.name;
+         // Je crée l'image dynamiquement avec les informations dans l'API
+         const imageProduit = document.createElement("img");
+         imageProduit.setAttribute('src', product.imageUrl);
+         imageProduit.setAttribute('alt', product.altTxt)
+         document.querySelector('.item__img').appendChild(imageProduit);
+         // Je remplace dynamiquement le nom, le prix et la description du produit
+         document.getElementById('title').innerText = product.name;
+         document.getElementById('price').innerText = product.price;
+         document.getElementById('description').innerText = product.description;
 
-    // Pareil pour le prix dans le span et la description dans le paragraphe
-    document.getElementById('price').innerText = product.price;
-    document.getElementById('description').innerText = product.description;
+         // Je boucle chaque couleur dans le tableau de la clé "colors" pour y crée une option de plus dans la liste
+         product.colors.forEach(function (color) {
+            const newOption = document.createElement('option');
+            newOption.setAttribute('value', color);
+            newOption.innerText = color;
+            // Je rajoute le ou les options dans l'élements select du document
+            document.getElementById('colors').appendChild(newOption);
+         });
+      }
+      updatePage(product)
 
-    // Je boucle chaque couleur dans le tableau de la clé "colors" pour y crée une option de plus dans la liste
-    product.colors.forEach(function(color) {
-      const newOption = document.createElement('option');
-      newOption.setAttribute('value', color);
-      newOption.innerText = color;
+      document.getElementById('addToCart').addEventListener("click", function () {
+         const selectColors = document.getElementById("colors");
+         // Je prends la valeur de l'index de la sélection de couleur et la valeur de la quantité en la transformant en Number
+         const optionSelected = selectColors.options[selectColors.selectedIndex].value;
+         const quantitySelected = Number(document.getElementById("quantity").value);
 
-      // Je rajoute le ou les options dans l'élements select du document
-      document.getElementById('colors').appendChild(newOption);  
-  });
+         function validateCartInput() {
 
+            // si l'utilisateur a fait au moins une de ces erreurs, le panier est invalidé et on bloque la suite :
+            if (
+               optionSelected === "" || // si aucune couleur n'a été sélectionnée
+               quantitySelected <= 0 || // si la quantité est inférieur ou égal à 0
+               quantitySelected > 100 || // si la quantité est supérieur à 100
+               !Number.isInteger(quantitySelected) // si la quantité est un nombre entier
+            ) {
+               alert("Vous n'avez pas correctement rempli votre panier, veuillez suivre les instructions");
+               return false; // la fonction renvoie "false" si une erreur est détectée 
 
-const buttonAddCart = document.getElementById('addToCart'); 
-
-let articleId = params.get("id");
-
-// Je vérifie si il y a déjà un panier dans le localstoarge : si oui, je le transforme en objet JS, sinon je le crée en tableau vide
-let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier')) : [];
-
-
-buttonAddCart.addEventListener("click", function() {
-  
-  const selectColors = document.getElementById("colors");
-  
-  // Je prends la valeur de l'index de la sélection de couleur et la valeur de la quantité en la transformant en Number
-  const optionSelected = selectColors.options[selectColors.selectedIndex].value;
-  const quantitySelected = Number(document.getElementById("quantity").value);
-
-  // si l'utilisateur a fait au moins une de ces erreurs, le panier est invalidé et on bloque la suite :
-  if (
-    optionSelected === "" || // si la couleur a été sélectionné
-    quantitySelected <= 0 || // si la quantité est inférieur ou égal à 0
-    quantitySelected > 100 || // si la quantité est supérieur à 100
-    !Number.isInteger(quantitySelected) // si la quantité est un nombre entier
-  ) {
-    alert("Vous n'avez pas correctement rempli votre panier, veuillez suivre les instructions");
-    return;
-  }
-
-  // je crée mon article qui sera un objet avec les proprités de cet article
-  let article = {
-    id: articleId,
-    option: optionSelected,
-    quantity: quantitySelected
-  };
-
-  // J'initalise une variable pour savoir si l'article existe, on part du principe que de base l'article n'existe pas
-  let articleExist = false;
+            } else {
+               return true;
+            }
+         }
 
 
-  // Je fais une boucle pour vérifier si l'article existe, si il existe la valeur de articleExist passe à true et sa quantité augmente
-  for (let i = 0; i < panier.length; i++) {
-    if (panier[i].id === articleId && panier[i].option === optionSelected) {
-      panier[i].quantity += quantitySelected;
-      articleExist = true;
-      break;
-    }
-  }
+         if (!validateCartInput()) { // si validateCartInput retourne false, on arrête le script
+            return;
+          }
 
-  // si l'article n'existe pas, on le push dans le tableau panier
-  if (!articleExist) {
-    panier.push(article);
-  }
 
-  localStorage.setItem("panier", JSON.stringify(panier));
+         // je crée mon article qui sera un objet avec les proprités de cet article
+         let article = {
+            id: productId,
+            option: optionSelected,
+            quantity: quantitySelected
+         };
 
-  console.log(panier);
-});
+         let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier')) : [];
 
-})
-  .catch(error => console.error(error));
-  
+         function isArticleExist(productId, optionSelected, quantitySelected, article) {
+            // J'initalise une variable pour savoir si l'article existe, on part du principe que de base l'article n'existe pas
+            let articleExist = false;
+            
+
+            // Je fais une boucle pour vérifier si l'article existe, si oui : la valeur de articleExist passe à true et sa quantité augmente
+            for (let i = 0; i < panier.length; i++) {
+               if (panier[i].id === productId && panier[i].option === optionSelected) {
+                  panier[i].quantity += quantitySelected;
+                  articleExist = true;
+                  alert(`Article et option déjà existante, la quantité de cet article a été ajoutée au panier !`);
+                  break; // On arrête la boucle dès qu'on a trouvé l'article dans le panier, car il ne peut y en avoir qu'un seul identique
+               }
+            }
+
+            // si l'article n'existe pas, on le push dans le tableau panier
+            if (!articleExist) {
+               panier.push(article)
+               alert('Nouvel article ajouté au panier !');
+            }
+         }
+
+         isArticleExist(productId, optionSelected, quantitySelected, article);
+         localStorage.setItem("panier", JSON.stringify(panier));
+
+         console.log(panier);
+      });
+
+   })
+   .catch(error => console.error(error));
